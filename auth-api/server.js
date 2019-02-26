5e0dfaffbdc82f620eb80a59aba9b7cce140b822
KiranmayiG <gkiranmayi97@gmail.com>
2019-02-26 14:06:58 +0530

    html connected

--- auth-api/server.js
+++ auth-api/server.js
@@ -1,25 +1,38 @@
 var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var publicThings = require(__dirname + '/routes/publicThings.js');
-var protectedThings = require(__dirname + '/routes/protectedThings.js');
+//var publicThings = require(__dirname + '/routes/publicThings.js');
+//var protectedThings = require(__dirname + '/routes/protectedThings.js');
 var users = require(__dirname + '/routes/users.js');
 var app;
 var router;
 var port = 3000;
+var path= require('path');
 
 app = express();
 
 app.use(morgan('combined')); //logger
 app.use(bodyParser.json());
 
-router = express.Router();
+// router = express.Router();
+// router.get('/public_things', publicThings.get);
+// router.get('/protected_things', protectedThings.get);
+// router.post('/users', users.post);
+// app.use('/api', router);
 
-router.get('/public_things', publicThings.get);
-router.get('/protected_things', protectedThings.get);
-router.post('/users', users.post);
+app.use(express.static(__dirname + '/public'));
 
-app.use('/api', router);
+app.get('/index', function (req, res) {
+res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/contact', function (req, res) {
+res.sendFile(path.join(__dirname + '/public/contact.html'));
+});
+
+app.get('/blog', function (req, res) {
+res.sendFile(path.join(__dirname + '/public/blog.html'));
+});
 
 app.listen(port, function() {
     console.log('Web server listening on localhost:' + port);
