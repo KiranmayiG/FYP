96acfeb5e54954a56d20c95641c17901662b29bf
KiranmayiG <gkiranmayi97@gmail.com>
2019-02-28 12:47:54 +0530

    added session

--- auth-api/server.js
+++ auth-api/server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
-var oracleDbStore = require('express-oracle-session')(session);
+//var oracleDbStore = require('express-oracle-session')(session);
 //var publicThings = require(__dirname + '/routes/publicThings.js');
 //var protectedThings = require(__dirname + '/routes/protectedThings.js');
 var users = require(__dirname + '/routes/users.js');
@@ -13,12 +13,12 @@ var port = 3000;
 var path= require('path');
 var oracledb = require('oracledb');
 var config = require(__dirname + '/config.js');
-
+var sess;
 app = express();
 
 app.use(morgan('combined')); //logger
 app.use(bodyParser.json());
-
+app.use(session({secret: 'ssshhhhh'}));
 // router = express.Router();
 // router.get('/public_things', publicThings.get);
 // router.get('/protected_things', protectedThings.get);
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+//app.use(cookieParser());
 
 app.get('/index', function (req, res) {
 res.sendFile(path.join(__dirname + '/public/index.html'));
@@ -63,42 +63,16 @@ app.post('/login', function(req, res) {
 
                     if(result.rows.length == 1)
                     {
-                          // initialize express-session to allow us track the logged-in user across sessions.
-                        //  req.cookies.user_sid= result.rows[0][0]);
-                        var options = {
-                          user:"fyp",
-                          password: 'qwertyui',
-                          connectString: 'localhost:1521/orcl'
-                      };
 
-                      var sessionStore = new oracleDbStore(options);
+                      var user_sid= result.rows[0][0];
 
-                      app.use(session({
-                          key: 'session_cookie_name',
-                          secret: 'session_cookie_secret',
-                          store: sessionStore,
-                          resave: true,
-                          saveUninitialized: true
-                      }));
-
-                      //     app.use((req, res, next) => {
-                      //       if (req.cookies.user_sid && !req.session.user) {
-                      //           res.clearCookie('user_sid');
-                      //       }
-                      //       next();
-                      //   });
-                      //
-                      // //  middleware function to check for logged-in users
-                      //   var sessionChecker = (req, res, next) => {
-                      //       if (req.session.user && req.cookies.user_sid) {
-                      //           res.redirect('/index');
-                      //       } else {
-                      //           next();
-                      //       }
-                      //     }
+                      sess = req.session;
+                      sess.username = req.body.username;
+                      res.redirect('/index');
+                    //  res.end('done ' + sess.username );
                }
 
-                    //res.send("The length is " + result.rows.length);
+               else res.send("Invalid Credentials. try again");
                  });
       }
   )
